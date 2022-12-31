0dac24addbc2d8893ce5e7ce2737faf63076dfdd
davide sbalzer <sbalzerdavide@hotmail.it>
2022-12-31 14:06:57 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  pages: {
+    index:{
+      entry: 'src/main.js',
+      template: 'index.html'
+    },
+  },
+
 })
