61e63f304e2800b3642954ae1028ddb951bf1591
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-05 23:40:39 +0200

    Add VisitaGinecologica and VisitaOstetrica pages

---
+++ src/pages/Visita-ostetrica.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'VisitaOstetrica',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<template>
+  <div class="visita-ginecologica">
+    <h1>Visita ostetrica</h1>
+    <p>Questa è la pagina per la visita ginecologica.</p>
+    <p>Qui puoi inserire i dati specifici per la visita ginecologica.</p>
+  </div>
+</template>
