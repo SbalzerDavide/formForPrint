d0e270a26e124b380ac929dc685db45bd54942f6
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-08 07:32:06 +0200

    feat: add doppler constant

---
+++ src/const/doppler.js
@@ -0,0 +1,54 @@
+export const doppler = [
+  {
+    text: "PI - Uterina destra",
+    name: "PIUDX",
+    value: "",
+    unit: "",
+    percentile: null,
+    ecoType: ["1T", "2T", "3T", "CA"],
+    incisura: null
+  },
+  {
+    text: "PI - Uterina sinistra",
+    name: "PIUSX",
+    value: "",
+    unit: "",
+    percentile: null,
+    ecoType: ["1T", "2T", "3T", "CA"],
+    incisura: null
+  },
+  // {
+  //   text: "RI - Uterine",
+  //   name: "RIU",
+  //   value: "",
+  //   unit: "",
+  //   percentile: null,
+  //   ecoType: ["1T", "2T", "3T", "CA"]
+  // },
+  {
+    text: "PI - Ombellicale",
+    name: "PIO",
+    value: "",
+    unit: "",
+    percentile: null,
+    ecoType: ["1T", "2T", "3T", "CA"],
+    incisura: null
+  },
+  {
+    text: "Cerebrale media (MCA)",
+    name: "MCA",
+    value: "",
+    unit: "",
+    MoM: null,
+    ecoType: ["1T", "2T", "3T", "CA"]
+  },
+  {
+    text: "Cerebrale media (MCA) - PI",
+    name: "MCAPI",
+    value: "",
+    unit: "",
+    percentile: null,
+    mean: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+]
