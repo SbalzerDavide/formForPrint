34ffacff8641f045fbaeead07d76370e1b96dc62
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-08 07:31:40 +0200

    feat: add anatomy constants

---
+++ src/const/anatomy.js
@@ -0,0 +1,178 @@
+export const anatomy = [
+  {
+    name: "Cranio encefalo",
+    value: "cranio",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Stomaco",
+    value: "stomaco",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Vescica",
+    value: "vescica",
+    checked: false,
+    ecoType: ["1T", "2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Parete addominale",
+    value: "addome",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Arti superiori",
+    value: "artiS",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Arti inferiori",
+    value: "artiI",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "3 vasi nel cordone",
+    value: "cordone",
+    checked: false,
+    ecoType: ["1T"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Trigono del ventricolo laterale distale",
+    value: "trigono",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Cavo del setto pellucido",
+    value: "cavo",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Cervelletto",
+    value: "cervelletto",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Cisterna magna",
+    value: "cisterna",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Orbite",
+    value: "orbite",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Labbro superiore",
+    value: "labbro",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Colonna vertebrale",
+    value: "colonna",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Situs cardiaco",
+    value: "situs",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "4 camere cardiache",
+    value: "4Camere",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Efflussi cardiaci sinistro e destro",
+    value: "eflussi",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Parete addominale anteriore",
+    value: "pareteAddominale",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Bolla gastirica",
+    value: "bolla",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Reni",
+    value: "reni",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Ossa lunghe degli arti superiori e inferiori",
+    value: "ossaLunghe",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+  {
+    name: "Mani e piedi presenti",
+    value: "maniPiedi",
+    checked: false,
+    ecoType: ["2T", "3T", "CA"],
+    comment: "",
+    showComment: false
+  },
+]
