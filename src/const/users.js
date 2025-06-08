8d89160253c71fb2db859449d908c30b148f03a5
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-08 07:32:14 +0200

    feat: add initial users data

---
+++ src/const/users.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    name: "Giorgia",
+    surname: "Mazzoni",
+    gender: "F",
+    signature: "",
+    email: "",
+    id: "",
+    operatorId: "221235"
+  },
+  {
+    name: "Claudia",
+    surname: "Maggi",
+    gender: "F",
+    signature: "",
+    email: "",
+    id: "",
+    operatorId: "221235"
+  }
+]
