5e0d64a2e2743bede054b7bb90d4d8c566a7c3ed
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-08 07:31:52 +0200

    feat: add biometriaFetale constants

---
+++ src/const/biometriaFetale.js
@@ -0,0 +1,101 @@
+export const biometriaFetale = [
+  {
+    text: "Diametro biparietale (BPD)",
+    name: "BPD",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["1T", "2T", "3T", "CA"]
+  },
+  {
+    text: "Lunghezza vertice-sacro (CRL)",
+    name: "CRL",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["1T"]
+  },
+  {
+    text: "Translucenza nucale (NT)",
+    name: "NT",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    over95: false,
+    delta: 0,
+    ecoType: ["1T"]
+  },
+  {
+    text: "Frequenza cardiaca fetale",
+    name: "FCF",
+    value: "",
+    unit: "bpm",
+    percentile: null,
+    ecoType: ["1T", "2T"]
+  },
+  {
+    text: "Circonferenza cranica (CC)",
+    name: "CC",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Cisterna magna (CM)",
+    name: "CM",
+    value: "",
+    unit: "mm",
+    right: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Ventricolo posteriore (VP)",
+    name: "VP",
+    value: "",
+    unit: "mm",
+    right: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Circonferenza addominale (CA)",
+    name: "CA",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Lungh, Femore (LF)",
+    name: "LF",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Omero",
+    name: "LO",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Cervelletto",
+    name: "TCD",
+    value: "",
+    unit: "mm",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+  {
+    text: "Stima del peso fetale (Hadlock)",
+    name: "HADLOCK",
+    calc: true,
+    value: "",
+    unit: "g",
+    percentile: null,
+    ecoType: ["2T", "3T", "CA"]
+  },
+]
