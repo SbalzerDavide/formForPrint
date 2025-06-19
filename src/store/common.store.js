82e4c0ff6ce9f493767b027ec4dc841f34d428b8
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-20 00:06:32 +0200

    feat: restructure common store with user, office, and ecoTool states and mutations

--- src/store/common.store.js
+++ src/store/common.store.js
@@ -2,11 +2,39 @@ import Vuex from "vuex"
 
 export const commonStore = new Vuex.Store({
   state: {
-    count: 1
+    name: '',
+    surname: '',
+    dateOfBirth: '',
+    age: null,
+    height: 0,
+    normalWeight: 0,
+    actualWeight: 0,
+    bmi: 0,
+    user: {},
+    office: {},
+    ecoTool: 'Samsung WS80',
+
   },
   mutations: {
-    increment(state) {
-      state.count++
+    setOperator(state, operator) {
+      state.user = operator
+    },
+    setOffice(state, office) {
+      state.office = office
+    },
+    setEcoTool(state, ecoTool) {
+      state.ecoTool = ecoTool
+    }
+  },
+  getters: {
+    user: (state) => {
+      return state.user
+    },
+    office: (state) => {
+      return state.office
+    },
+    ecoTool: (state) => {
+      return state.ecoTool
     }
   }
 })
