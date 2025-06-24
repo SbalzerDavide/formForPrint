36d2380ff55b034755374632949288cb60bc7a75
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-06-24 23:24:36 +0200

    manage office data

--- src/store/common.store.js
+++ src/store/common.store.js
@@ -1,40 +1,59 @@
 import Vuex from "vuex"
+import { Users } from '@/const/users.js'
+import { Offices } from '@/const/offices.js'
+
 
 export const commonStore = new Vuex.Store({
   state: {
-    name: '',
-    surname: '',
-    dateOfBirth: '',
-    age: null,
-    height: 0,
-    normalWeight: 0,
-    actualWeight: 0,
-    bmi: 0,
-    user: {},
-    office: {},
+    // office 
+    user: Users[0], // Default user
+    office: Offices[0], // Default office
     ecoTool: 'Samsung WS80',
+
+    // patient 
+    // name: '',
+    // surname: '',
+    // dateOfBirth: '',
+    // age: null,
+    // height: 0,
+    // normalWeight: 0,
+    // actualWeight: 0,
+    // bmi: 0,
 
   },
   mutations: {
-    setOperator(state, operator) {
-      state.user = operator
+    SET_USER(state, user) {
+      console.log(user);
+
+      state.user = user
     },
-    setOffice(state, office) {
+    SET_OFFICE(state, office) {
+      if (office.id === 'pralboino') {
+        state.ecoTool = 'Samsung HS50'
+      } else if (office.id === 'desenzano') {
+        state.ecoTool = 'Samsung WS80'
+      }
       state.office = office
     },
-    setEcoTool(state, ecoTool) {
+    SET_ECO_TOOL(state, ecoTool) {
       state.ecoTool = ecoTool
+    },
+    SET_NAME(state, name) {
+      state.name = name
+    },
+    SET_SURNAME(state, surname) {
+      state.surname = surname
     }
   },
-  getters: {
-    user: (state) => {
-      return state.user
-    },
-    office: (state) => {
-      return state.office
-    },
-    ecoTool: (state) => {
-      return state.ecoTool
-    }
-  }
+  // getters: {
+  //   user: (state) => {
+  //     return state.user
+  //   },
+  //   office: (state) => {
+  //     return state.office
+  //   },
+  //   ecoTool: (state) => {
+  //     return state.ecoTool
+  //   }
+  // }
 })
