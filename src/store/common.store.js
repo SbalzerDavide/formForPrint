61427d47189eca676a1967029b2e01acc433bf1e
Davide Sbalzer <davide.sbalzer@neosperience.com>
2025-07-09 22:14:23 +0200

    refactor: uncomment patient state properties and mutations for better clarity

--- src/store/common.store.js
+++ src/store/common.store.js
@@ -11,20 +11,20 @@ export const commonStore = new Vuex.Store({
     ecoTool: 'Samsung WS80',
 
     // patient 
-    // name: '',
-    // surname: '',
-    // dateOfBirth: '',
-    // age: null,
-    // height: 0,
-    // normalWeight: 0,
-    // actualWeight: 0,
-    // bmi: 0,
+    name: '',
+    surname: '',
+    dateOfBirth: '',
+    age: null,
+    height: 0,
+    normalWeight: 0,
+    actualWeight: 0,
+    bmi: 0,
+
+    patientMoreText: '',
 
   },
   mutations: {
     SET_USER(state, user) {
-      console.log(user);
-
       state.user = user
     },
     SET_OFFICE(state, office) {
@@ -38,11 +38,41 @@ export const commonStore = new Vuex.Store({
     SET_ECO_TOOL(state, ecoTool) {
       state.ecoTool = ecoTool
     },
+
     SET_NAME(state, name) {
       state.name = name
     },
     SET_SURNAME(state, surname) {
       state.surname = surname
+    },
+    SET_DATE_OF_BIRTH(state, dateOfBirth) {
+      let today = dayjs()
+      let formattedDate = dayjs(dateOfBirth)
+      const age = today.diff(formattedDate, 'year')
+      state.dateOfBirth = formattedDate
+      state.age = age
+    },
+    SET_HEIGHT(state, height) {
+      const meterHeight = height / 100
+      const bmi = parseFloat(state.actualWeight / (meterHeight * meterHeight)).toFixed(2)
+
+      state.height = height
+      state.bmi = bmi
+    },
+    SET_NORMAL_WEIGHT(state, normalWeight) {
+      state.normalWeight = normalWeight
+    },
+    SET_ACTUAL_WEIGHT(state, actualWeight) {
+      const meterHeight = state.height / 100
+      const bmi = parseFloat(actualWeight / (meterHeight * meterHeight)).toFixed(2)
+
+      state.actualWeight = actualWeight
+      state.bmi = bmi
+    },
+    SET_PATIENT_MORE_TEXT(state, patientMoreText) {
+      state.patientMoreText = patientMoreText
+      console.log('SET_PATIENT_MORE_TEXT', state.patientMoreText);
+
     }
   },
   // getters: {
