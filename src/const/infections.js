89368b0e0b58f84ad8550bb6db0b7fabca61747f
Davide Sbalzer <sbalzerdavide@hotmail.it>
2025-12-31 15:36:32 +0100

    feat: add infections data structure for medical records

---
+++ src/const/infections.js
@@ -0,0 +1,29 @@
+export const Infections = [
+  {
+    label: 'TOXO',
+    value: 'neg'
+  },
+  {
+    label: 'RUBEO',
+    value: 'neg'
+  },
+  {
+    label: 'HBsAg',
+    value: 'neg'
+  },
+  {
+    label: 'HCV',
+    value: 'neg'
+  },
+  {
+    label: 'HIV',
+    value: 'neg'
+  },
+  {
+    label: 'TPHA',
+    value: 'neg'
+  }, {
+    label: 'CMV',
+    value: 'neg'
+  }
+]
